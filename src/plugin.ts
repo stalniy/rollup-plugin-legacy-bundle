869ec3670f62ebda02e4fc02e3ac85e1b903cb30
Sergii <sergiy.stotskiy@gmail.com>
2020-03-10 12:46:42 +0200

    refactor(plugin): extracts resolveFromBundle plugin into separete file

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,50 +1,5 @@
-import {
-  rollup, OutputBundle, OutputOptions, Plugin,
-} from 'rollup';
-
-type BundleResolverOptions = {
-  polyfills?: string[] | undefined | null
-};
-
-function importPolyfills(polyfills: BundleResolverOptions['polyfills']): string {
-  if (!polyfills) {
-    return '';
-  }
-
-  return `${polyfills.map((id) => `import '${id}'`).join(';')};`;
-}
-
-function resolveFromBundle(
-  bundle: OutputBundle,
-  options: BundleResolverOptions = {},
-): Plugin {
-  return {
-    name: 'resolve-bundle',
-    resolveId(id) {
-      if (bundle[id]) {
-        return id;
-      }
-    },
-    load(id) {
-      const chunk = bundle[id];
-
-      if (!chunk || chunk.type !== 'chunk') {
-        return;
-      }
-
-      let { code } = chunk;
-
-      if (chunk.isEntry) {
-        code = importPolyfills(options.polyfills) + code;
-      }
-
-      return {
-        code,
-        map: chunk.map,
-      };
-    },
-  };
-}
+import { rollup, OutputOptions, Plugin } from 'rollup';
+import { resolveFromBundle, BundleResolverOptions } from './resolveBundlePlugin';
 
 type LegacyPluginOptions = Omit<OutputOptions, 'plugins'> & {
   polyfills?: BundleResolverOptions['polyfills'],
---
+++ src/resolveBundlePlugin.ts
@@ -0,0 +1,47 @@
+import { Plugin, OutputBundle } from 'rollup';
+
+export type BundleResolverOptions = {
+  polyfills?: string[] | undefined | null
+};
+
+function importPolyfills(polyfills: BundleResolverOptions['polyfills']): string {
+  if (!polyfills) {
+    return '';
+  }
+
+  return `${polyfills.map((id) => `import '${id}'`).join(';')};`;
+}
+
+export function resolveFromBundle(
+  bundle: OutputBundle,
+  options: BundleResolverOptions = {},
+): Plugin {
+  return {
+    name: 'resolve-bundle',
+    resolveId(id) {
+      if (bundle[id]) {
+        return id;
+      }
+    },
+    load(id) {
+      const chunk = bundle[id];
+
+      if (!chunk || chunk.type !== 'chunk') {
+        return;
+      }
+
+      if (!chunk.isEntry) {
+        return {
+          code: chunk.code,
+          map: chunk.map,
+        };
+      }
+
+      return {
+        // TODO: use magic-string or similar to properly handle sourcemaps
+        code: importPolyfills(options.polyfills) + chunk.code,
+        map: chunk.map,
+      };
+    },
+  };
+}
